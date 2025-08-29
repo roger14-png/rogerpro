5e79d917b2703208315640427c81741466c4ef59
roger14-png <malobarodgers66@gmail.com>
2025-08-29 19:05:39 +0300

    Add files via upload
    My final Plp project with both mobile view

---
+++ App.tsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
+const App: React.FC = () => {
+  return (
+    <HashRouter>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </HashRouter>
+  );
+};
+
+export default App;
---
+++ types.ts
@@ -0,0 +1,12 @@
+
+export interface SliderImage {
+  src: string;
+  alt: string;
+}
+
+export interface TeamMember {
+  name: string;
+  role: string;
+  bio: string;
+  avatarUrl: string;
+}
